7db188f1179f5a3d828381b411e7d53048f8265a
akemimadoka <jxhzq1996@126.com>
2017-09-30 18:19:01 +0800

    修改错误处理

--- NatsuLang.ASTInterpreter/Main.cpp
+++ NatsuLang.ASTInterpreter/Main.cpp
@@ -3,6 +3,64 @@
 using namespace NatsuLib;
 using namespace NatsuLang;
 
+void PrintException(natLog& logger, std::exception const& e);
+
+void PrintException(natLog& logger, natException const& e)
+{
+	logger.LogErr(u8"捕获到来自函数 {0}({1}:{2})的异常，描述为 {3}"_nv, e.GetSource(), e.GetFile(), e.GetLine(), e.GetDesc());
+
+#ifdef EnableStackWalker
+	logger.LogErr(u8"调用栈为"_nv);
+
+	const auto& stackWalker = e.GetStackWalker();
+
+	for (std::size_t i = 0, count = stackWalker.GetFrameCount(); i < count; ++i)
+	{
+		const auto& symbol = stackWalker.GetSymbol(i);
+#ifdef _WIN32
+		logger.LogErr(u8"{3}: (0x%p) {4} (地址：0x%p) (文件 {5}:{6} (地址：0x%p))"_nv, symbol.OriginalAddress, reinterpret_cast<const void*>(symbol.SymbolAddress), reinterpret_cast<const void*>(symbol.SourceFileAddress), i, symbol.SymbolName, symbol.SourceFileName, symbol.SourceFileLine);
+#else
+		logger.LogErr(u8"0x%p : {1}"_nv, symbol.OriginalAddress, symbol.SymbolInfo);
+#endif
+	}
+#endif
+
+	try
+	{
+		std::rethrow_if_nested(e);
+	}
+	catch (natException& inner)
+	{
+		logger.LogErr(u8"由以下异常引起："_nv);
+		PrintException(logger, inner);
+	}
+	catch (std::exception& inner)
+	{
+		logger.LogErr(u8"由以下异常引起："_nv);
+		PrintException(logger, inner);
+	}
+}
+
+void PrintException(natLog& logger, std::exception const& e)
+{
+	logger.LogErr(u8"捕获到异常，描述为 {0}"_nv, e.what());
+
+	try
+	{
+		std::rethrow_if_nested(e);
+	}
+	catch (natException& inner)
+	{
+		logger.LogErr(u8"由以下异常引起："_nv);
+		PrintException(logger, inner);
+	}
+	catch (std::exception& inner)
+	{
+		logger.LogErr(u8"由以下异常引起："_nv);
+		PrintException(logger, inner);
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc > 2)
@@ -53,13 +111,13 @@ int main(int argc, char* argv[])
 	}
 	catch (natException& e)
 	{
-		logger.LogErr(e.GetDesc());
+		PrintException(logger, e);
 		logger.LogErr(u8"解释器由于未处理的不可恢复的异常而中止运行，请按 Enter 退出程序");
 		console.ReadLine();
 	}
 	catch (std::exception& e)
 	{
-		logger.LogErr(e.what());
+		PrintException(logger, e);
 		logger.LogErr(u8"解释器由于未处理的不可恢复的异常而中止运行，请按 Enter 退出程序");
 		console.ReadLine();
 	}
