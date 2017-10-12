9f1100e017ad4a35e99613cec1b6a711cb1375a6
akemimadoka <jxhzq1996@126.com>
2017-10-12 12:18:25 +0800

    摸鱼

--- NatsuLang.AOTCompiler/CodeGen.cpp
+++ NatsuLang.AOTCompiler/CodeGen.cpp
@@ -1,4 +1,4 @@
-#include "CodeGen.h"
+﻿#include "CodeGen.h"
 
 #include <llvm/Target/TargetMachine.h>
 #include <llvm/Target/TargetOptions.h>
@@ -8,7 +8,327 @@
 
 using namespace NatsuLib;
 using namespace NatsuLang;
-using namespace NatsuLang::Compiler;
+using namespace Compiler;
+
+AotCompiler::AotAstConsumer::AotAstConsumer(AotCompiler& compiler)
+	: m_Compiler{ compiler }
+{
+}
+
+AotCompiler::AotAstConsumer::~AotAstConsumer()
+{
+}
+
+void AotCompiler::AotAstConsumer::Initialize(ASTContext& context)
+{
+}
+
+void AotCompiler::AotAstConsumer::HandleTranslationUnit(ASTContext& context)
+{
+}
+
+nBool AotCompiler::AotAstConsumer::HandleTopLevelDecl(Linq<Valued<Declaration::DeclPtr>> const& decls)
+{
+	for (auto decl : decls)
+	{
+		if (const auto funcDecl = static_cast<natRefPointer<Declaration::FunctionDecl>>(decl))
+		{
+
+		}
+	}
+
+	return true;
+}
+
+AotCompiler::AotStmtVisitor::AotStmtVisitor(AotCompiler& compiler, natRefPointer<Declaration::FunctionDecl> funcDecl)
+	: m_Compiler{ compiler }, m_CurrentFunction{ std::move(funcDecl) }, m_CurrentFunctionValue{ nullptr },
+	  m_LastVisitedValue{ nullptr }
+{
+}
+
+AotCompiler::AotStmtVisitor::~AotStmtVisitor()
+{
+}
+
+void AotCompiler::AotStmtVisitor::VisitBreakStmt(natRefPointer<Statement::BreakStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitCatchStmt(natRefPointer<Statement::CatchStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitTryStmt(natRefPointer<Statement::TryStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitCompoundStmt(natRefPointer<Statement::CompoundStmt> const& stmt)
+{
+	for (auto&& item : stmt->GetChildrens())
+	{
+		Visit(item);
+	}
+}
+
+void AotCompiler::AotStmtVisitor::VisitContinueStmt(natRefPointer<Statement::ContinueStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitDeclStmt(natRefPointer<Statement::DeclStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitDoStmt(natRefPointer<Statement::DoStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitExpr(natRefPointer<Expression::Expr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitConditionalOperator(natRefPointer<Expression::ConditionalOperator> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitArraySubscriptExpr(natRefPointer<Expression::ArraySubscriptExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitBinaryOperator(natRefPointer<Expression::BinaryOperator> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitCompoundAssignOperator(natRefPointer<Expression::CompoundAssignOperator> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitBooleanLiteral(natRefPointer<Expression::BooleanLiteral> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitConstructExpr(natRefPointer<Expression::ConstructExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitDeleteExpr(natRefPointer<Expression::DeleteExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitNewExpr(natRefPointer<Expression::NewExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitThisExpr(natRefPointer<Expression::ThisExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitThrowExpr(natRefPointer<Expression::ThrowExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitCallExpr(natRefPointer<Expression::CallExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitMemberCallExpr(natRefPointer<Expression::MemberCallExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitCastExpr(natRefPointer<Expression::CastExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitAsTypeExpr(natRefPointer<Expression::AsTypeExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitImplicitCastExpr(natRefPointer<Expression::ImplicitCastExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitCharacterLiteral(natRefPointer<Expression::CharacterLiteral> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitDeclRefExpr(natRefPointer<Expression::DeclRefExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitFloatingLiteral(natRefPointer<Expression::FloatingLiteral> const& expr)
+{
+	const auto floatType = static_cast<natRefPointer<Type::BuiltinType>>(expr->GetExprType());
+
+	if (floatType->GetBuiltinClass() == Type::BuiltinType::Float)
+	{
+		m_LastVisitedValue = llvm::ConstantFP::get(m_Compiler.m_LLVMContext, llvm::APFloat{ static_cast<nFloat>(expr->GetValue()) });
+	}
+	else
+	{
+		m_LastVisitedValue = llvm::ConstantFP::get(m_Compiler.m_LLVMContext, llvm::APFloat{ expr->GetValue() });
+	}
+}
+
+void AotCompiler::AotStmtVisitor::VisitIntegerLiteral(natRefPointer<Expression::IntegerLiteral> const& expr)
+{
+	const auto intType = static_cast<natRefPointer<Type::BuiltinType>>(expr->GetExprType());
+	const auto typeInfo = m_Compiler.m_AstContext.GetTypeInfo(intType);
+
+	m_LastVisitedValue = llvm::ConstantInt::get(m_Compiler.m_LLVMContext, llvm::APInt{ typeInfo.Size * 8, expr->GetValue(), intType->IsSigned() });
+}
+
+void AotCompiler::AotStmtVisitor::VisitMemberExpr(natRefPointer<Expression::MemberExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitParenExpr(natRefPointer<Expression::ParenExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitStmtExpr(natRefPointer<Expression::StmtExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitStringLiteral(natRefPointer<Expression::StringLiteral> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitUnaryExprOrTypeTraitExpr(natRefPointer<Expression::UnaryExprOrTypeTraitExpr> const& expr)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitUnaryOperator(natRefPointer<Expression::UnaryOperator> const& expr)
+{
+	Visit(expr->GetOperand());
+
+	const auto input = m_LastVisitedValue;
+	// TODO: 加载 input
+	auto value = input;
+
+	const auto opCode = expr->GetOpcode();
+
+	switch (opCode)
+	{
+	case Expression::UnaryOperationType::PostInc:
+	case Expression::UnaryOperationType::PreInc:
+		value = m_Compiler.m_IRBuilder.CreateAdd(value, llvm::ConstantInt::get(value->getType(), 1));
+		m_LastVisitedValue = opCode == Expression::UnaryOperationType::PostInc ? value : input;
+		break;
+	case Expression::UnaryOperationType::PostDec:
+	case Expression::UnaryOperationType::PreDec:
+		value = m_Compiler.m_IRBuilder.CreateAdd(value, llvm::ConstantInt::get(value->getType(), -1));
+		m_LastVisitedValue = opCode == Expression::UnaryOperationType::PostInc ? value : input;
+		break;
+	case Expression::UnaryOperationType::Plus:
+		m_LastVisitedValue = value;
+		break;
+	case Expression::UnaryOperationType::Minus:
+		value = m_Compiler.m_IRBuilder.CreateNeg(value);
+		m_LastVisitedValue = value;
+		break;
+	case Expression::UnaryOperationType::Not:
+		value = m_Compiler.m_IRBuilder.CreateNot(value);
+		m_LastVisitedValue = value;
+		break;
+	case Expression::UnaryOperationType::LNot:
+		value = m_Compiler.m_IRBuilder.CreateNot(value);
+		m_LastVisitedValue = value;
+		break;
+	default:
+		assert(!"Invalid opCode");
+		[[fallthrough]];
+	case Expression::UnaryOperationType::Invalid:
+		nat_Throw(AotCompilerException, u8"错误的操作码"_nv);
+	}
+}
+
+void AotCompiler::AotStmtVisitor::VisitForStmt(natRefPointer<Statement::ForStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitGotoStmt(natRefPointer<Statement::GotoStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitIfStmt(natRefPointer<Statement::IfStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitLabelStmt(natRefPointer<Statement::LabelStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitNullStmt(natRefPointer<Statement::NullStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitReturnStmt(natRefPointer<Statement::ReturnStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitSwitchCase(natRefPointer<Statement::SwitchCase> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitCaseStmt(natRefPointer<Statement::CaseStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitDefaultStmt(natRefPointer<Statement::DefaultStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitSwitchStmt(natRefPointer<Statement::SwitchStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitWhileStmt(natRefPointer<Statement::WhileStmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
+
+void AotCompiler::AotStmtVisitor::VisitStmt(natRefPointer<Statement::Stmt> const& stmt)
+{
+	nat_Throw(AotCompilerException, u8"此功能尚未实现"_nv);
+}
 
 AotCompiler::AotCompiler(natLog& logger)
 	: m_DiagConsumer{ make_ref<AotDiagConsumer>(*this) },
@@ -49,7 +369,7 @@ std::unique_ptr<llvm::Module> AotCompiler::Compile(Uri const& uri)
 
 	const llvm::TargetOptions opt;
 	const llvm::Optional<llvm::Reloc::Model> RM{};
-	const auto machine = target->createTargetMachine(targetTriple, "generic", "", opt, RM);
+	const auto machine = target->createTargetMachine(targetTriple, "generic", "", opt, RM, llvm::None, llvm::CodeGenOpt::Default);
 	m_Module->setDataLayout(machine->createDataLayout());
 
 	// TODO
